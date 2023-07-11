77289fdb4afdc62d78c2be30d2cb56e103f5fdf9
Johnny Wang <johnny8906030714@gmail.com>
2023-07-12 02:14:53 +0800

    nav is done udemy not yet

--- src/components/nav-component.js
+++ src/components/nav-component.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Outlet, Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBookOpen } from "@fortawesome/free-solid-svg-icons";
@@ -9,47 +9,113 @@ import { faBagShopping } from "@fortawesome/free-solid-svg-icons";
 import logo from "../images/logo.jpg";
 
 const Nav = () => {
+  let [currentPage, setCurrentPage] = useState(1);
+  const choosePage = (page) => {
+    setCurrentPage(page);
+  };
+  window.addEventListener("scroll", () => {
+    let header = document.querySelector("header");
+    if (window.scrollY == 0) {
+      header.style.boxShadow = "";
+    } else {
+      header.style.boxShadow = "0 10px 6px -6px #777";
+    }
+  });
   return (
-    <div>
-      <header>
-        <section className="logo">
-          <img src={logo} alt="網站logo" />
-          <h1>Johnny Wang</h1>
-        </section>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">首頁</Link>
-            </li>
-            <li>
-              <Link to="/about">
+    <header>
+      <section className="logo">
+        <img src={logo} alt="網站logo" />
+        <h1>Johnny Wang</h1>
+      </section>
+      <nav>
+        <ul>
+          <li>
+            {currentPage == 1 && (
+              <Link
+                to="/"
+                onClick={() => {
+                  choosePage(1);
+                }}
+                className="current-page"
+              >
+                首頁
+              </Link>
+            )}
+            {currentPage != 1 && (
+              <Link
+                to="/"
+                onClick={() => {
+                  choosePage(1);
+                }}
+              >
+                首頁
+              </Link>
+            )}
+          </li>
+          <li>
+            {currentPage == 2 && (
+              <Link
+                to="/about"
+                onClick={() => {
+                  choosePage(2);
+                }}
+                className="current-page"
+              >
                 <FontAwesomeIcon icon={faUser} /> 關於Johnny
               </Link>
-            </li>
-            {/* <li>
+            )}
+            {currentPage != 2 && (
+              <Link
+                to="/about"
+                onClick={() => {
+                  choosePage(2);
+                }}
+              >
+                <FontAwesomeIcon icon={faUser} /> 關於Johnny
+              </Link>
+            )}
+          </li>
+          {/* <li>
               <Link to="/udemy">
                 <FontAwesomeIcon icon={faBookOpen} /> 2023家教課程
               </Link>
             </li> */}
-            <li>
-              <Link to="/udemy">
+          <li>
+            {currentPage == 3 && (
+              <Link
+                to="/udemy"
+                onClick={() => {
+                  choosePage(3);
+                }}
+                className="current-page"
+              >
                 <FontAwesomeIcon icon={faLaptopCode} /> udemy課程
               </Link>
-            </li>
-            {/* <li>
+            )}
+            {currentPage != 3 && (
+              <Link
+                to="/udemy"
+                onClick={() => {
+                  choosePage(3);
+                }}
+              >
+                <FontAwesomeIcon icon={faLaptopCode} /> udemy課程
+              </Link>
+            )}
+          </li>
+          {/* <li>
               <Link to="/pj1">
                 <FontAwesomeIcon icon={faFileLines} /> 深碗專題專案
               </Link>
             </li> */}
-            {/* <li>
+          {/* <li>
               <Link to="/about">
                 <FontAwesomeIcon icon={faBagShopping} /> 電商平台專案
               </Link>
             </li> */}
-          </ul>
-        </nav>
-      </header>
-    </div>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
--- src/components/udemy-component.js
+++ src/components/udemy-component.js
@@ -7,7 +7,7 @@ const UdemyComponent = () => {
     <div className="udemy-container">
       <ul>
         <li>
-          <h2>前端網頁開發-HTMLCSS</h2>
+          <h2>前端網頁開發-HTML&CSS</h2>
           <div className="card-container">
             <div className="card" style={{ width: "18rem" }}>
               <img src={pj1} className="card-img-top" alt="圍棋資訊網" />
@@ -83,14 +83,15 @@ const UdemyComponent = () => {
               {/* </ul> */}
             </div>
             <div className="card" style={{ width: "18rem" }}>
-              <img src={pj1} className="card-img-top" alt="圍棋資訊網" />
+              <img src={pj1} className="card-img-top" alt="日本旅遊網站" />
               <div className="card-body">
-                <h5 className="card-title">圍棋資訊網</h5>
+                <h5 className="card-title">日本旅遊網站</h5>
                 <p clasNames="card-text">
-                  利用該專題熟悉HTML元素、屬性之運用，並搭配基礎的CSS排版美化介面。
+                  熟練前端排版、切版，並深刻考慮RWD，利用flex排版以及media
+                  query解決專案問題。
                 </p>
                 <a
-                  href="../../project1/index.html"
+                  href="../../project2/index.html"
                   className="btn btn-primary"
                   target="_blank"
                 >
@@ -156,14 +157,14 @@ const UdemyComponent = () => {
               {/* </ul> */}
             </div>
             <div className="card" style={{ width: "18rem" }}>
-              <img src={pj1} className="card-img-top" alt="圍棋資訊網" />
+              <img src={pj1} className="card-img-top" alt="自我介紹網站" />
               <div className="card-body">
-                <h5 className="card-title">圍棋資訊網</h5>
+                <h5 className="card-title">自我介紹網站</h5>
                 <p clasNames="card-text">
-                  利用該專題熟悉HTML元素、屬性之運用，並搭配基礎的CSS排版美化介面。
+                  熟悉前端外部插件如boostrap、cdn.js 中的fontawsome等等。
                 </p>
                 <a
-                  href="../../project1/index.html"
+                  href="../../project3/index.html"
                   className="btn btn-primary"
                   target="_blank"
                 >
@@ -227,6 +228,72 @@ const UdemyComponent = () => {
               </div>
 
               {/* </ul> */}
+            </div>
+            {/* <div className="card" style={{ width: "18rem" }}>
+              <img src={pj1} className="card-img-top" alt="圍棋資訊網" />
+              <div className="card-body">
+                <h5 className="card-title">圍棋資訊網</h5>
+                <p clasNames="card-text">
+                  利用該專題熟悉HTML元素、屬性之運用，並搭配基礎的CSS排版美化介面。
+                </p>
+                <a
+                  href="../../project1/index.html"
+                  className="btn btn-primary"
+                  target="_blank"
+                >
+                  跳至該專案頁面
+                </a>
+              </div>
+
+              <button
+                type="button"
+                className="btn btn-light"
+                style={{ borderRadius: "0" }}
+                data-bs-toggle="modal"
+                data-bs-target="#exampleModal"
+              >
+                更新資訊
+              </button>
+
+              <div
+                className="modal fade"
+                id="exampleModal"
+                tabindex="-1"
+                aria-labelledby="exampleModalLabel"
+                aria-hidden="true"
+              >
+                <div className="modal-dialog">
+                  <div className="modal-content">
+                    <div className="modal-header">
+                      <h5 className="modal-title" id="exampleModalLabel">
+                        Modal title
+                      </h5>
+                      <button
+                        type="button"
+                        className="btn-close"
+                        data-bs-dismiss="modal"
+                        aria-label="Close"
+                      ></button>
+                    </div>
+                    <div className="modal-body">
+                      Close Close Close Close Close Close Close Close Close
+                      Close Close Close Close Close Close
+                    </div>
+                    <div className="modal-footer">
+                      <button
+                        type="button"
+                        className="btn btn-secondary"
+                        data-bs-dismiss="modal"
+                      >
+                        Close
+                      </button>
+                      <button type="button" className="btn btn-primary">
+                        Save changes
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
             <div className="card" style={{ width: "18rem" }}>
               <img src={pj1} className="card-img-top" alt="圍棋資訊網" />
@@ -244,10 +311,6 @@ const UdemyComponent = () => {
                 </a>
               </div>
 
-              {/* <ul class="list-group list-group-flush">*/}
-              {/* <li class="list-group-item">更新資訊</li> */}
-
-              {/* 更新資訊按鈕 */}
               <button
                 type="button"
                 className="btn btn-light"
@@ -258,7 +321,6 @@ const UdemyComponent = () => {
                 更新資訊
               </button>
 
-              {/* 更新資訊彈出視窗 */}
               <div
                 className="modal fade"
                 id="exampleModal"
@@ -298,82 +360,7 @@ const UdemyComponent = () => {
                   </div>
                 </div>
               </div>
-
-              {/* </ul> */}
-            </div>
-            <div className="card" style={{ width: "18rem" }}>
-              <img src={pj1} className="card-img-top" alt="圍棋資訊網" />
-              <div className="card-body">
-                <h5 className="card-title">圍棋資訊網</h5>
-                <p clasNames="card-text">
-                  利用該專題熟悉HTML元素、屬性之運用，並搭配基礎的CSS排版美化介面。
-                </p>
-                <a
-                  href="../../project1/index.html"
-                  className="btn btn-primary"
-                  target="_blank"
-                >
-                  跳至該專案頁面
-                </a>
-              </div>
-
-              {/* <ul class="list-group list-group-flush">*/}
-              {/* <li class="list-group-item">更新資訊</li> */}
-
-              {/* 更新資訊按鈕 */}
-              <button
-                type="button"
-                className="btn btn-light"
-                style={{ borderRadius: "0" }}
-                data-bs-toggle="modal"
-                data-bs-target="#exampleModal"
-              >
-                更新資訊
-              </button>
-
-              {/* 更新資訊彈出視窗 */}
-              <div
-                className="modal fade"
-                id="exampleModal"
-                tabindex="-1"
-                aria-labelledby="exampleModalLabel"
-                aria-hidden="true"
-              >
-                <div className="modal-dialog">
-                  <div className="modal-content">
-                    <div className="modal-header">
-                      <h5 className="modal-title" id="exampleModalLabel">
-                        Modal title
-                      </h5>
-                      <button
-                        type="button"
-                        className="btn-close"
-                        data-bs-dismiss="modal"
-                        aria-label="Close"
-                      ></button>
-                    </div>
-                    <div className="modal-body">
-                      Close Close Close Close Close Close Close Close Close
-                      Close Close Close Close Close Close
-                    </div>
-                    <div className="modal-footer">
-                      <button
-                        type="button"
-                        className="btn btn-secondary"
-                        data-bs-dismiss="modal"
-                      >
-                        Close
-                      </button>
-                      <button type="button" className="btn btn-primary">
-                        Save changes
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              {/* </ul> */}
-            </div>
+            </div> */}
           </div>
         </li>
         <li>
